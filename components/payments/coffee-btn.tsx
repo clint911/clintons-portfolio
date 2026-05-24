39f97ee4776c60d4d47faceed2a6c24a12797f91
clint911 <clingyking007@gmail.com>
2026-05-24 22:21:09 +0300

    updating payments for bmac to test suby

---
+++ components/payments/coffee-btn.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CoffeeButton() {
+  return (
+    <a
+      href="https://checkout.suby.fi/p/pro_u7jdcicih3xzl4xdigcqxrrd"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block w-full bg-yellow-400 border-3 border-black py-3 text-center text-xl font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transform hover:translate-x-1 hover:translate-y-1 hover:shadow-none transition-all"
+    >
+      <span>Buy me a matcha latte 🍵</span>
+    </a>
+  );
+}
